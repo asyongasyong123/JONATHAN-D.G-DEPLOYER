88e9b118f8854aca49f022fb8eb9ab92ae735e4c
asyongasyong123 <asyongasyong123@gmail.com>
2026-06-11 21:34:22 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 set -e
-
-# Ibutang ang PORT gikan sa Cloud Run ngadto sa Nginx
-sed -i "s/listen .*;/listen ${PORT:-8080};/g" /usr/local/openresty/nginx/conf/nginx.conf
-
-# Sugdi ang Nginx ug Xray
+sed -i "s/\${PORT:-8080}/$PORT/g" /usr/local/openresty/nginx/conf/nginx.conf
 openresty -g 'daemon off;' &
 exec xray run -c /etc/xray.json
