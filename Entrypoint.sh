bcc9bc593810b463d6a5f385f58ab85f534be722
asyongasyong123 <asyongasyong123@gmail.com>
2026-06-11 18:02:46 +0800

    Create Entrypoint.sh

---
+++ Entrypoint.sh
@@ -0,0 +1,15 @@
+# =========================
+# ENTRYPOINT.SH
+# =========================
+
+cat > entrypoint.sh <<EOF
+#!/bin/sh
+
+/usr/local/bin/xray run -c /etc/xray.json &
+
+sleep 3
+
+exec /usr/local/openresty/bin/openresty -g 'daemon off;'
+EOF
+
+chmod +x entrypoint.sh
