6d1cb7a396b5d962c671823f2355718db6c83d6d
asyongasyong123 <asyongasyong123@gmail.com>
2026-06-11 21:39:07 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 set -e
-sed -i "s/\${PORT:-8080}/$PORT/g" /usr/local/openresty/nginx/conf/nginx.conf
+
+# Direkta nga ilisi ang port sa Nginx config
+sed -i "s|listen.*;|listen ${PORT:-8080};|g" /usr/local/openresty/nginx/conf/nginx.conf
+
+# Siguroha nga ang Nginx modungog sa tanang IP
+sed -i "s|listen ${PORT:-8080};|listen 0.0.0.0:${PORT:-8080};|g" /usr/local/openresty/nginx/conf/nginx.conf
+
+# Ipakita ang port para makita sa log
+echo "Starting services on port: ${PORT:-8080}"
+
+# Sugdi ang Nginx sa foreground
 openresty -g 'daemon off;' &
+
+# Sugdi ang Xray
 exec xray run -c /etc/xray.json
